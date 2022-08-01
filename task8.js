eb33386081cb3dc6a96401c18a9ef896df00cb24
devbiirds <vadikoff57@gmail.com>
2022-08-01 19:41:23 +0300

    solving task8

---
+++ task8.js
@@ -0,0 +1,21 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Введите дату в формате ДД.ММ.ГГГГ :", function (date) {
+    date = new Date(date)s;
+    let days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+    console.log(days[date.getDay()]);
+    rl.close();
+});
+
+
+var date = new Date();
+var now = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0);
+var result = Math.floor((date.getTime() - now.getTime()) / 1000 / 60);
+
+console.log(result)
